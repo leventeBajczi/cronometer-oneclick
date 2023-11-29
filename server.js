554e581567144ff3e7ecec42eaf66698de38ed57
Levente Bajczi <bajczilevi@gmail.com>
2023-11-29 14:51:34 +0100

    Patched call

--- server.js
+++ server.js
@@ -91,7 +91,9 @@ function addData(nonce, id, amount, dateStr) {
         const month = date.getMonth() + 1;
         const year = date.getFullYear();
         // const data = `7|0|9|https://cronometer.com/cronometer/|3F3D68A6C8269C1940D0F9F4147958AE|com.cronometer.shared.rpc.CronometerService|addServing|java.lang.String/2004016611|com.cronometer.shared.entries.models.Serving/955192269|I|${nonce}|com.cronometer.shared.entries.models.Day/782579793|1|2|3|4|3|5|6|7|8|6|9|${day}|${month}|${year}|0|0|0|${(amount*100).toFixed(0)}|${id.food}|A|${id.serving}|3|0|4677888|`;
-        const data = `7|0|9|https://cronometer.com/cronometer/|82CCD30578522199DE0F5458B8D07285|com.cronometer.shared.rpc.CronometerService|addServing|java.lang.String/2004016611|com.cronometer.shared.entries.models.Serving/2553599101|I|${nonce}|com.cronometer.shared.entries.models.Day/782579793|1|2|3|4|3|5|6|7|8|6|9|${day}|${month}|${year}|1|1|0|4|0|0|${(amount*100).toFixed(0)}|${id.food}|A|${id.serving}|0|0|4677888|`
+        // const data = `7|0|9|https://cronometer.com/cronometer/|82CCD30578522199DE0F5458B8D07285|com.cronometer.shared.rpc.CronometerService|addServing|java.lang.String/2004016611|com.cronometer.shared.entries.models.Serving/2553599101|I|${nonce}|com.cronometer.shared.entries.models.Day/782579793|1|2|3|4|3|5|6|7|8|6|9|${day}|${month}|${year}|1|1|0|4|0|0|${(amount*100).toFixed(0)}|${id.food}|A|${id.serving}|0|0|4677888|`
+        const data = `7|0|12|https://cronometer.com/cronometer/|948B95EFABE5D3BF9B07D85F5A49865C|com.cronometer.shared.rpc.CronometerService|updateDiary|java.lang.String/2004016611|I|java.util.List|${nonce}|java.util.Collections$SingletonList/1586180994|com.cronometer.shared.entries.changes.AddEntryChange/3949104564|com.cronometer.shared.entries.models.Serving/2553599101|com.cronometer.shared.entries.models.Day/782579793|1|2|3|4|3|5|6|7|8|4677888|9|10|1|1|11|12|${day}|${month}|${year}|1|1|0|3|0|0|${(amount*100).toFixed(0)}|${id.food}|A|${id.serving}|0|0|`
+           
         console.log(data);
         const options = {
         hostname: 'cronometer.com',
